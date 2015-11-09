96f7bf3aa4d290921bf8a7c89d08998ce1530996
Joseph <joseph@Sage.local>
2015-11-09 15:20:48 -0600

    ...

---
+++ example3/Applications.js
@@ -0,0 +1,23 @@
+// import React from 'react';
+// import FuelSavingsResults from './FuelSavingsResults';
+// import Calculator from '../businessLogic/fuelSavingsCalculator';
+
+'use strict';
+class Application extends React.Component {
+// constructor(props) {
+//     super(props);
+//     this.state = {}
+//     };
+// }
+
+
+
+    render() {
+        return (
+            <div>Fuel Savings Analysis</div>
+        );
+        
+    }
+}
+
+// export default FuelSavingsCalculatorForm;
